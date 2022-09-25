ec7437f1505b7e6ccf50ba95fa9c73f115f4f0cc
pavan6115 <kulkarnipavan6115@gmail.com>
2022-09-26 00:27:20 +0530

    [feat]: added & styled pagenotfoundpage

---
+++ src/pages/PageNotFound/PageNotFound.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './PageNotFound.css'
+
+export const PageNotFound = () => {
+  return (
+    <div>
+      <div className='title'>
+        <Link to='/'>
+          <span className='title-1'>Auto</span>
+          <span className='title-2'>mophile</span>
+        </Link>
+      </div>
+      <div className='error-container'>
+        <div className='error-msg'>
+          <div className='txt-404'>
+            <span className='title-2'>4</span>
+            <span>0</span>
+            <span className='title-2'>4</span>
+          </div>
+          <div className='txt'>Whoooops!</div>
+          <p className='txt'>Seems like you have lost your way
+          <div></div>To return to homepage, click below
+          </p>
+          <Link to='/'>
+            <button className='btn-homepage'>Go Home</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
