7f533bdc7a520d9afb0f8808caaac781620ec1ee
pavan6115 <kulkarnipavan6115@gmail.com>
2022-09-25 23:56:03 +0530

    [feat]: added pages & routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
-import "./App.css";
-import logo from "./logo.png";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import {
+  HomePage,
+  LoginPage,
+  MoviePage,
+  PageNotFound,
+  RacingPage,
+  ReviewPage,
+  SeriesPage,
+  SignUpPage,
+} from './pages'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/movies' element={<MoviePage />}></Route>
+        <Route path='/series' element={<SeriesPage />}></Route>
+        <Route path='/racing' element={<RacingPage />}></Route>
+        <Route path='/reviews' element={<ReviewPage />}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
+        <Route path='/signup' element={<SignUpPage />}></Route>
+        <Route path='*' element={<PageNotFound />}></Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
