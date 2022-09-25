d30fea05fce26d330f54ba4db36846a4a86c315a
pavan6115 <kulkarnipavan6115@gmail.com>
2022-09-25 23:58:29 +0530

    [feat]: added router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { makeServer } from "./server";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import './index.css'
+import { makeServer } from './server'
 
 // Call make Server
-makeServer();
+makeServer()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
