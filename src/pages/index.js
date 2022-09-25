9aa97f3ee126ac3c9e82811ff9b6ec58b1d3f3a6
pavan6115 <kulkarnipavan6115@gmail.com>
2022-09-26 00:28:17 +0530

    [feat]: barrel export

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { HomePage } from './HomePage/HomePage'
+export { LoginPage } from './LoginPage/LoginPage'
+export { MoviePage } from './MoviesPage/MoviePage'
+export { PageNotFound } from './PageNotFound/PageNotFound'
+export { RacingPage } from './RacingPage/RacingPage'
+export { ReviewPage } from './ReviewsPage/ReviewPage'
+export { SeriesPage } from './SeriesPage/SeriesPage'
+export { SignUpPage } from './SignUpPage/SignUpPage'
