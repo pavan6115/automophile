988766467a3b9a3346b22b8e484ca50b9f45f82a
pavan6115 <kulkarnipavan6115@gmail.com>
2022-09-29 13:37:04 +0530

    [feat]: added data provider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
+import { DataProvider } from './context/data/data-context'
 import './index.css'
 import { makeServer } from './server'
 
@@ -11,7 +12,9 @@ makeServer()
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <DataProvider>
+        <App />
+      </DataProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
