80e6c2e7ff4fb60d0c11c94d76e607677c2ff7c6
pavan6115 <kulkarnipavan6115@gmail.com>
2022-09-29 13:40:18 +0530

    [feat]: added data prop

--- src/components/HorizontalCard/HCard.js
+++ src/components/HorizontalCard/HCard.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { BsPlay } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+import { useData } from '../../context/data/data-context'
 import './HCard.css'
 
-export const HCard = ({ thumbnail, title, time }) => {
+export const HCard = ({ thumbnail, title, time, data }) => {
+  const { dispatchVideos } = useData()
+  const navigate = useNavigate()
+
+  const sendData = (info) => {
+    dispatchVideos({ type: 'DATA', payload: info})
+    navigate(`/video/${info._id}`)
+  }
+
   return (
     <div className='hcard-container'>
       <div className='hcard'>
         <img src={thumbnail} alt='title' className='hcard-img' />
         <div className='bg-linear'>
           <div className='movie-info'>
-            <BsPlay className='play-btn' />
+            <BsPlay className='play-btn' onClick={() => sendData(data)} />
             <div className='hcard-title'>{title}</div>
             <div className='movie-duration'>{time}</div>
           </div>
