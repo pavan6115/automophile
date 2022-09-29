933c292e9bed975038c305a5f234794afc0a03bd
pavan6115 <kulkarnipavan6115@gmail.com>
2022-09-29 13:44:05 +0530

    [feat]: custom hook title & api data call

--- src/pages/MoviesPage/MoviePage.js
+++ src/pages/MoviesPage/MoviePage.js
@@ -1,40 +1,31 @@
 import React from 'react'
-import moviepicsum from '../../assets/moviepicsum.jpeg'
 import { Footer, HCard, Navbar } from '../../components'
+import { useData } from '../../context/data/data-context'
+import { PageTitle } from '../../hooks/PageTitle/PageTitle'
 import './MoviePage.css'
 
 export const MoviePage = () => {
+  const { videosState } = useData()
+  const moviesDB = videosState.videos[0][1]
+
+  PageTitle('Movies | Automophile')
+
   return (
     <div>
       <Navbar />
       <div className='movie-container'>
         <h2 className='movie-title'>Movies</h2>
         <div className='movie-card-container'>
-          <HCard
-            thumbnail={moviepicsum}
-            title={'Ford VS Ferrari'}
-            time={'2 hr 10 min'}
-          />
-          <HCard
-            thumbnail={moviepicsum}
-            title={'Ford VS Ferrari'}
-            time={'2 hr 10 min'}
-          />
-          <HCard
-            thumbnail={moviepicsum}
-            title={'Ford VS Ferrari'}
-            time={'2 hr 10 min'}
-          />
-          <HCard
-            thumbnail={moviepicsum}
-            title={'Ford VS Ferrari'}
-            time={'2 hr 10 min'}
-          />
-          <HCard
-            thumbnail={moviepicsum}
-            title={'Ford VS Ferrari'}
-            time={'2 hr 10 min'}
-          />
+          {moviesDB.map((movie) => {
+            return (
+              <HCard
+                thumbnail={movie.thumbnail}
+                title={movie.title}
+                time={movie.duration}
+                data={movie}
+              />
+            )
+          })}
         </div>
       </div>
       <Footer />
