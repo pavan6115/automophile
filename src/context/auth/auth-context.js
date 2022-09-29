518576f07066bf09aa22cb6557e912d5f89229fc
pavan6115 <kulkarnipavan6115@gmail.com>
2022-09-29 22:46:12 +0530

    [feat]: added auth context

---
+++ src/context/auth/auth-context.js
@@ -0,0 +1,85 @@
+import axios from 'axios'
+import React, { createContext, useContext, useState } from 'react'
+import toast from 'react-hot-toast'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+// @ts-ignore
+const AuthContext = createContext()
+
+const userProfile = JSON.parse(localStorage.getItem('userProfile')) || {
+  token: null,
+  user: null,
+}
+const AuthProvider = ({ children }) => {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const [user, setUser] = useState({
+    token: userProfile.token,
+    user: userProfile.user,
+  })
+
+  //   services
+  const login = async (email, password) => {
+    try {
+      console.log('inside try')
+      const loginResponse = await axios.post(`/api/auth/login`, {
+        email,
+        password,
+      })
+      console.log('response ', loginResponse)
+      if (loginResponse.status === 200) {
+        const userProfile = {
+          token: loginResponse.data.encodedToken,
+          user: loginResponse.data.foundUser,
+        }
+        setUser(userProfile)
+        localStorage.setItem('userProfile', JSON.stringify(userProfile))
+        navigate(location.state?.from?.pathname || '/', { replace: true })
+      }
+    } catch (err) {
+      toast.error('Something is wrong, Please try again 😔')
+      console.log('Login error : ', err)
+    }
+  }
+
+  const signup = async (email, password, firstName, lastName) => {
+    try {
+      const signUpResponse = await axios.post('/api/auth/signup', {
+        email,
+        password,
+        firstName,
+        lastName,
+      })
+      if (signUpResponse.status === 201) {
+        const userProfile = {
+          token: signUpResponse.data.encodedToken,
+          user: signUpResponse.data.createdUser,
+        }
+        setUser(userProfile)
+        localStorage.setItem('userProfile', JSON.stringify(userProfile))
+        navigate(location.state?.from?.pathname || '/', { replace: true })
+      }
+    } catch (e) {
+      toast.error('Signup error, try again!')
+      console.log('Signup error: ', e)
+    }
+  }
+
+  const logout = () => {
+    localStorage.removeItem('userProfile')
+    setUser({ token: null, user: null })
+    toast.success('Logout Successful')
+    navigate('/')
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, signup, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+const useAuth = () => useContext(AuthContext)
+
+export { useAuth, AuthProvider }
